75e57ffa03c8a1417e83f96485a16c84e74bad8e
Arush Mishra <arushmish.15@gmail.com>
2026-01-08 14:31:00 +0530

    Update read.py
    Added error handling for file not found exception.

--- read.py
+++ read.py
@@ -1,10 +1,13 @@
+try:
+  file_handler = open("sample.txt", "rt")
+  
+  l1 = file_handler.readline()
+  l2 = file_handler.readline()
+  
+  file_handler.close()
+  print("Reading file content:")
+  print(f"line 1: {l1}")
+  print(f"line 2: {l2}")
 
-file_handler = open("sample.txt", "rt")
-
-l1 = file_handler.readline()
-l2 = file_handler.readline()
-
-file_handler.close()
-print("Reading file content:")
-print(f"line 1: {l1}")
-print(f"line 2: {l2}")
+except FileNotFoundError:
+    print("Error: the file'sample.txt' was not found. ")
